45314195fae6922c648b488bf89011848222ad62
Dan Burton <danburton.email@gmail.com>
2020-10-03 23:38:24 -0400

    console.log the board

--- index.mjs
+++ index.mjs
@@ -1,6 +1,84 @@
 import {loadStdlib} from '@reach-sh/stdlib';
 import * as backend from './build/index.main.mjs';
 
+const queens = ['♕', '♛'];
+const chessSymbols = [
+  // W_KING, W_BISHOP, W_KNIGHT,  W_ROOK,
+  '♔', '♗', '♘', '♖',
+  // W_PAWN1, W_PAWN2, W_PAWN3, W_PAWN4,
+  '♙', '♙', '♙', '♙',
+  // B_KING, B_BISHOP, B_KNIGHT, B_ROOK,
+  '♚', '♝', '♞', '♜',
+  // B_PAWN1, B_PAWN2, B_PAWN3, B_PAWN4,
+  '♟', '♟', '♟', '♟',
+];
+const pieces = 16;
+function isWhitePiece(piece) {
+  return piece < (pieces / 2);
+}
+
+// const [isPlayer, WHITE, BLACK] = makeEnum(players);
+const WHITE = 0;
+const BLACK = 1;
+
+// const [isPieceStatus, ALIVE, PROMOTED, DEAD] = makeEnum(pieceStatuses);
+const ALIVE = 0;
+const PROMOTED = 1;
+const DEAD = 2;
+
+function displayPiece(piece, status) {
+  if (status == PROMOTED) {
+    return queens[isWhitePiece(piece) ? WHITE : BLACK];
+  } else {
+    return chessSymbols[piece];
+  }
+}
+
+function displayTile(tile) {
+  tile = tile || ' '
+  return ` ${tile} |`;
+}
+
+function displayState(state) {
+  const boards = 2;
+  const rows = 8;
+  const cols = 4;
+  const toIndex = ({row, col, board}) => (
+    board * (rows * cols) + row * cols + col
+  );
+
+  const abcd = '    A   B   C   D  ';
+  const hr   = '  -----------------';
+  const tiles = Array(boards * cols * rows).fill(null);
+  for (const piece in state.pieces) {
+    const ppos = state.pieces[piece];
+    if (ppos.pieceStatus != DEAD) {
+      tiles[toIndex(ppos.pos)] = displayPiece(piece, ppos.status);
+    }
+  }
+
+  console.log();
+  for (const board in Array(boards).fill(null)) {
+    const lines = [];
+    let line;
+    for (const row in Array(rows).fill(null)) {
+      line = `${parseInt(row) + 1} |`;
+      for (const col in Array(cols).fill(null)) {
+        const idx = toIndex({row, col, board});
+        line += displayTile(tiles[idx]);
+      }
+      lines.unshift(hr);
+      lines.unshift(line);
+    }
+    lines.unshift(hr);
+    lines.unshift(`     Board ${board == 0 ? 'A' : 'B'}`);
+    lines.push(abcd);
+    lines.push('');
+    lines.push('');
+    console.log(lines.join('\n'));
+  }
+}
+
 (async () => {
   const stdlib = await loadStdlib();
   const {
